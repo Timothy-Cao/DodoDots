d794ece72825031852de7dfd21eca9b9c2d85da2
timothy.cao <timothy.cao@bytedance.com>
2026-04-19 23:44:56 -0700

    feat(visual): shared SVG bloom filters

---
+++ components/game/BloomDefs.tsx
@@ -0,0 +1,21 @@
+export function BloomDefs() {
+  return (
+    <defs>
+      <filter id="bloom-dim" x="-50%" y="-50%" width="200%" height="200%">
+        <feGaussianBlur stdDeviation="1.5" result="blur" />
+        <feMerge>
+          <feMergeNode in="blur" />
+          <feMergeNode in="SourceGraphic" />
+        </feMerge>
+      </filter>
+      <filter id="bloom-bright" x="-50%" y="-50%" width="200%" height="200%">
+        <feGaussianBlur stdDeviation="4" result="blur" />
+        <feMerge>
+          <feMergeNode in="blur" />
+          <feMergeNode in="blur" />
+          <feMergeNode in="SourceGraphic" />
+        </feMerge>
+      </filter>
+    </defs>
+  );
+}
